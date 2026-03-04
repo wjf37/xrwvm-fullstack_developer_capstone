5beb61892d810980ca4e35a27a527ef9861dc1f7
wjf37 <vincentw3007@gmail.com>
2026-03-04 12:41:07 -0500

    added carmodel carmake models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,7 +22,16 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if (count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
