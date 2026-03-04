ceea32b518a5d5db1a1eeeda6846efde6b72641f
wjf37 <vincentw3007@gmail.com>
2026-03-04 13:40:40 -0500

    render dealership table

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -89,7 +89,7 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-def get_dealerships(request):
+def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
+import Dealers from "./components/Dealers/Dealers"
+import Dealer from "./components/Dealers/Dealer"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
