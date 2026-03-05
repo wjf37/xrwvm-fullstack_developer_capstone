4e5218793b502742bec204bc4e837a41ae6fe391
wjf37 <vincentw3007@gmail.com>
2026-03-05 10:11:58 -0500

    containerized

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -31,7 +31,7 @@ def get_request(endpoint, **kwargs):
 
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"/analyze/"+text
+    request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
